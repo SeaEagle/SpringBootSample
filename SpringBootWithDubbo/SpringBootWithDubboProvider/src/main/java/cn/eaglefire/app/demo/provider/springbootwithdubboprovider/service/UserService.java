c4f8f240f770b1b305d28655fbd8f7f2c9b6d991
SeaEagle <gentlemaneagle@163.com>
2016-11-02 15:01:03 +0800

    整理去除无关的文件

---
+++ SpringBootWithDubbo/SpringBootWithDubboProvider/src/main/java/cn/eaglefire/app/demo/provider/springbootwithdubboprovider/service/UserService.java
@@ -0,0 +1,15 @@
+package cn.eaglefire.app.demo.provider.springbootwithdubboprovider.service;
+
+import cn.eaglefire.app.demo.api.springbootwithdubboapi.bean.UserBean;
+
+/**
+ * Created by eagle on 2016/11/2.
+ */
+public class UserService implements cn.eaglefire.app.demo.api.springbootwithdubboapi.service.UserService {
+
+    @Override
+    public UserBean getUserById(UserBean userBean) {
+        return null;
+    }
+
+}
