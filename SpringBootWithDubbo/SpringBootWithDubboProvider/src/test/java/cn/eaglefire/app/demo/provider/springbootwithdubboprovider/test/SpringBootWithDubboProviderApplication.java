e4f618a4328dbfbcb3ba6cadeae0a86da4cc1a71
SeaEagle <gentlemaneagle@163.com>
2016-11-03 17:13:22 +0800

    新增测试环境

---
+++ SpringBootWithDubbo/SpringBootWithDubboProvider/src/test/java/cn/eaglefire/app/demo/provider/springbootwithdubboprovider/test/SpringBootWithDubboProviderApplication.java
@@ -0,0 +1,31 @@
+package cn.eaglefire.app.demo.provider.springbootwithdubboprovider.test;
+
+import com.alibaba.dubbo.container.Main;
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by eagle on 2016/11/3.
+ */
+@SpringBootApplication
+public class SpringBootWithDubboProviderApplication {
+
+    private static Logger logger = Logger.getLogger(SpringBootWithDubboProviderApplication.class);
+
+    public static void main(String[] args){
+
+        logger.info("SpringApplication Starting.");
+        SpringApplication.run(cn.eaglefire.app.demo.provider.springbootwithdubboprovider.SpringBootWithDubboProviderApplication.class, args);
+        logger.info("SpringApplication Finish.");
+
+//        logger.info("Dubbo Service Starting.");
+//        Main.main(args);
+//        logger.info("Dubbo Service Stop.");
+
+        //
+        //Runtime.getRuntime().addShutdownHook(new ApplicationShutdownHook());
+
+    }
+
+}
