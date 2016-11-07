e6a69135ec076f8f0ae05e00cfe6517fd632b3ef
SeaEagle <gentlemaneagle@163.com>
2016-11-07 11:19:52 +0800

    改造Dubbo为jar包自动加载方式

--- SpringBootWithDubbo/SpringBootWithDubboProviderPlain/src/main/java/cn/eaglefire/app/demo/provider/springbootwithdubboproviderplain/SpringBootWithDubboProviderPlainApplication.java
+++ SpringBootWithDubbo/SpringBootWithDubboProviderPlain/src/main/java/cn/eaglefire/app/demo/provider/springbootwithdubboproviderplain/SpringBootWithDubboProviderPlainApplication.java
@@ -3,7 +3,6 @@ package cn.eaglefire.app.demo.provider.springbootwithdubboproviderplain;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
 
@@ -11,7 +10,7 @@ import java.io.IOException;
  * Created by eagle on 2016/11/2.
  */
 @SpringBootApplication
-@ImportResource(locations = {"classpath:META-INF/dubboService.xml"})
+//@ImportResource(locations = {"classpath:META-INF/dubboService.xml"})
 public class SpringBootWithDubboProviderPlainApplication {
 
     private static Logger logger = Logger.getLogger(SpringBootWithDubboProviderPlainApplication.class);
--- SpringBootWithDubbo/SpringBootWithDubboStarter/src/main/java/cn/eaglefire/app/demo/starter/springbootwithdubbostarter/configuration/DubboAutoConfiguration.java
+++ SpringBootWithDubbo/SpringBootWithDubboStarter/src/main/java/cn/eaglefire/app/demo/starter/springbootwithdubbostarter/configuration/DubboAutoConfiguration.java
@@ -4,8 +4,8 @@ import cn.eaglefire.app.demo.starter.springbootwithdubbostarter.runner.DubboServ
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnNotWebApplication;
+import org.springframework.context.annotation.*;
 import org.springframework.core.annotation.Order;
 
 /**
@@ -13,6 +13,8 @@ import org.springframework.core.annotation.Order;
  */
 @Configuration
 @Order
+@PropertySource("classpath:dubbo.properties")
+@ImportResource(locations = {"classpath:META-INF/dubboService.xml"})
 public class DubboAutoConfiguration {
 
     protected static Logger logger = LoggerFactory.getLogger(DubboAutoConfiguration.class);
@@ -20,7 +22,14 @@ public class DubboAutoConfiguration {
     @Value("${shutdown.latch.domain.name: cn.eaglefire.app.demo.service.management}")
     private String shutdownLatchDomainName;
 
+    /**
+     * 启动JMX
+     *
+     * 排除spring-boot-starter-web场景下启动
+     * @return
+     */
     @Bean
+    @ConditionalOnNotWebApplication
     public DubboServiceLatchCommandLineRunner configureDubboServiceLatchCommandLineRunner(){
         logger.info("DubboAutoConfiguration enabled by adding DubboServiceLatchCommandLineRunner");
         DubboServiceLatchCommandLineRunner runner = new DubboServiceLatchCommandLineRunner();
